e0f055b27c1acd9212babcd891176664ff1b00dc
Administrador <administrador@OptiPlex-790.(none)>
2015-08-20 08:44:49 -0300

    Adicionado titulo e removido espaços

--- fatorial.js
+++ fatorial.js
@@ -1,25 +1,11 @@
 var url = require("url");
 var math = require('mathjs');
 exports.fatorial=function(req,res,params){
-
     console.log("Fatorial");
     console.log(params);
-    var n1 =  parseInt(params.n1)
-   
-  
+    var n1 =  parseInt(params.n1) 
     var resultado = math.factorial(n1)
     res.end("Fatorial: "+resultado);
-
      	res.writeHead(200, {'Content-Type': 'text/plain'});
    	res.end(''+resultado+'');
-  
-
-
-
-
-
-
-
-
-
 }
